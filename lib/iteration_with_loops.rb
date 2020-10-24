6d0c4851641a66011e23195705f7a9e1fbe420cf
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-24 16:09:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,13 +1,13 @@
 
 def find_even_values(src)
-count = 0
-while count < src.length do
-  inner_count = 0
-  while inner_count < src[count].length do
-    if(src[count][inner_count] % 2 == 0)
-      puts src[count][inner_count]
+  count = 0
+  while count < src.length do 
+    inner_count = 0
+    while inner_count < src[count].length do 
+      if(src[count][inner_count] % 2 == 0)
+        puts src[count][inner_count]
     end
     inner_count = inner_count + 1
-    count = count + 1
   end
+  count = count + 1
 end
